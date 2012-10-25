d854633dd828320fd4bd351baec711e64bf6cfba
meleyal <william.meleyal@gmail.com>
2012-10-25 11:31:03 +0200

    add test for underscore + backbone versions

--- test/backbone-on-rails_test.rb
+++ test/backbone-on-rails_test.rb
@@ -1,17 +1,21 @@
 require 'test_helper'
 
-class BackboneOnRailsTest < ActiveSupport::TestCase
+class BackboneOnRailsTest < Rails::Generators::TestCase
+
+  destination File.expand_path("../../tmp", __FILE__)
 
   def setup
     @app = Dummy::Application
   end
 
-  test "backbone.js is found as an asset" do
-    assert_not_nil @app.assets["backbone"]
-  end
-
   test "underscore.js is found as an asset" do
     assert_not_nil @app.assets["underscore"]
+    assert_file "#{BackboneOnRails::Engine.root}/vendor/assets/javascripts/underscore.js", /1.4.1/
+  end
+
+  test "backbone.js is found as an asset" do
+    assert_not_nil @app.assets["backbone"]
+    assert_file "#{BackboneOnRails::Engine.root}/vendor/assets/javascripts/backbone.js", /0.9.2/
   end
 
 end
