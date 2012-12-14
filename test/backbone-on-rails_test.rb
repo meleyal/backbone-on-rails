c416ed75bbbef20ba1bd1570d817aef9d02f6bf1
Nick Colgan <nick.colgan@gmail.com>
2012-12-14 02:44:38 -0600

    Upgrade backbone to v0.9.9

--- test/backbone-on-rails_test.rb
+++ test/backbone-on-rails_test.rb
@@ -15,7 +15,7 @@ class BackboneOnRailsTest < Rails::Generators::TestCase
 
   test "backbone.js is found as an asset" do
     assert_not_nil @app.assets["backbone"]
-    assert_file "#{BackboneOnRails::Engine.root}/vendor/assets/javascripts/backbone.js", /0.9.2/
+    assert_file "#{BackboneOnRails::Engine.root}/vendor/assets/javascripts/backbone.js", /0.9.9/
   end
 
 end
